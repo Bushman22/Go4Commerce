c9ad1318de090cb0035302ba7590b991226bea1b
keagan <keaganbush12@gmail.com>
2023-06-17 09:07:27 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,70 @@
+
+
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="src/routes/style.css">
+<title>Go4Commerce</title>
+<body>
+ <script src = "src/routes/dropdown.js"></script>
+<header class="header">
+    <nav class="navbar">
+        <logo><img src="\images\go4logo.png" alt="logo"></logo>
+        <ul class="nav-menu">
+            <li class="nav-item"><a href="/features" class="nav-link">Features & functionality</a></li>
+            <div class="dropdown">
+                <button onclick="myFunction1()" class="dropbtn">Components
+                    <i class="fa fa-caret-down"></i></button>
+               <div class="dropdown-content" id = "myDropdown">
+                  <a href="/admin">Admin Panel</a>
+                  <a href="/templates">Template Design</a>
+                  <a href="/intergrations">Integrations</a>
+                  <a href="/unified">Unified Shopping Cart</a>
+                  <a href="/payment">Payment Gateway</a>
+                </div>
+            </div>
+            
+            <div class="dropdown">
+                <button onclick="myFunction2()" class="dropbtn">About Us
+                    <i class="fa fa-caret-down"></i>
+                </button>
+               <div class="dropdown-content" id= "myDropdownbutton" >
+                  <a href="/team">Meet the Team</a>
+                  <a href="/resellers">Our Resellers</a>
+                </div>
+            </div>
+            <li class="nav-item"><a href="/contact" class="nav-link">Contact Us</a></li>
+            <li class="nav-item1"><a href="/" class="nav-link1">Book a Meeting</a></li>
+            <li class="nav-item2"><a href="/admin" class="nav-link1">Admin Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+<div class ="one">
+    <h1>DIGITIZE YOUR<br/> VISION</h1>
+    <p>For even the most sophisticated business models,
+         we<br/> provide an all-in-one, headless and multiple 
+        integration<br/> template-based platform. </p>
+   
+</div>
+<img src ="\images\Homepage banner illustration.png" alt="banner" style= margin-top:30px>
+
+
+</body>
+
+
+<style>
+    
+  @import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz@0,6..12;1,6..12&family=Questrial&display=swap');
+
+
+.one{
+    float:left;
+    justify-content: center;
+    text-align:left;
+    margin: 160px 130px;
+    
+}
+
+</style>
---
+++ src/routes/intergrations/+page.svelte
@@ -0,0 +1,59 @@
+
+<script>
+
+</script>
+<link rel="stylesheet" href="src/routes/style.css">
+<title>intergrations</title>
+<body>
+
+<header class="header">
+    <nav class="navbar">
+        <logo><img src="\images\go4logo.png" alt="logo"></logo>
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="/about" class="nav-link">Features & functionality</a>
+            </li>
+            <li class="nav-item">
+                <a href="/contact" class="nav-link">Components</a>
+            </li>
+            <li class="nav-item">
+                <a href="/contact" class="nav-link">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a href="/contact" class="nav-link">Contact Us</a>
+            </li>
+            
+            <li class="nav-item1">
+                <a href="/" class="nav-link1">Book a Meeting</a>
+            </li>
+            <li class="nav-item2">
+                <a href="/admin panel" class="nav-link1">Admin Login</a>
+            </li>
+        </ul>
+       
+    </nav>
+</header>
+
+
+
+
+
+
+
+
+
+</body>
+
+
+<style>
+
+
+
+
+
+
+
+
+
+
+</style>
