f7a20652f04810ca58088f0b3fe1e041fee7738a
keagan <keaganbush12@gmail.com>
2023-06-28 12:06:32 +0200

    edited payment and unified

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -45,12 +45,73 @@
         <img class= "img1" src ="\images\Payment Gateway.png" alt="banner">
         </div>
 
+        <div class = "multiple">
+            <img class ="multipleimage" src = "\images\MULTIPLE AND INTERNATIONAL image.jpg" alt = "multiple">
+            <div class = "multiplepara">
+                <h2>MULTIPLE AND<br/>INTERNATIONAL</h2>
+                <p>We create a custom connection to any payment gateway,<br/>
+                    no matter where you are in the world, so you can take<br/>
+                    payments online right away.</p>
+            </div>
+        </div>
 
+        <div class="check">
+            <div class="checkpara">
+                <h2>CHECKOUT<br/>PROCESS</h2>
+                 <ul>
+                    <li><p>Because our payment process has been<br/>tried and tested, you benefit from a higher<br/>conversion rate.</p></li>
+                    <li><h3>Conversion-optimized <br/>experience</h3></li>
+                    <p>The checkout block comes with everything<br/>
+                        you need to create a seamless checkout<br/>
+                        experience.</p>
+                        <li><h3>A Simple User Flow</h3></li>
+                        <p>Eliminate any points of friction in the<br/>
+                            purchasing process by clearly defined steps.</p>
+                 </ul>
+            </div>
+            <img class=" checkimage" src="\images\Checkout process illustration.jpg" alt="howitworks">
+        </div>
+
+        <div class="bottom">
+            <div class="bot">
+                <img class= "images" src ="\images\Multiple Currencies.jpg" alt="MC">
+                <h3>Multiple Currencies</h3>
+                <p>Offer your customers the option of viewing<br/>
+                    prices in their own currency.</p>
+            </div>
+
+            <div class="bot">
+                <img class= "images" src ="\images\Merchant.jpg" alt="MC">
+                <h3>Merchant</h3>
+                <p>Use your own Merchant to make payment<br/>
+                    directly into your account. </p>
+            </div>
+
+            <div class="bot">
+                <img class= "images" src ="\images\No Product Limits.jpg" alt="MC">
+                <h3>No Product Limits</h3>
+                <p>Customers are able to purchase as many<br/>
+                    products to the unified shopping cart as they<br/>
+                    desire. </p>
+            </div>
+        </div>
+
+        <div class = "footer">
+            <logo2><a href = "/"><img src="\images\go4logo.png" alt="logo" style=height:40px></a></logo2>
+        
+                        
+                        
+        </div>
 </body>
 
 
 <style>
-
+p{
+    padding-bottom: 10px;
+}
+h2{
+    padding-bottom: 20px;
+}
 .payment{
     display: flex;
     justify-content: space-around;
@@ -61,10 +122,53 @@
     padding-top: 80px;
 }
 
+.multiple{
+    display: flex;
+    justify-content: center;
+    padding-top: 120px;
+}
 
+.multipleimage{
+    padding-right:30px;
+}
+.multiplepara{
+    padding-top: 80px;
+}
 
+.check{
+    display:flex;
+    justify-content:space-evenly;
+    padding-top: 100px;
+}
 
+.checkpara{
+    padding-top: 60px;
+    padding-left:50px;
+}
 
+.checkpara li{
+    list-style-image: url('/images/Check mark.jpg');
+    
+}
+
+.checkimage{
+    height:550px;
+}
+
+.bottom{
+    padding-top: 100px;
+    padding-bottom: 100px;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+.bot{
+    text-align: center;
+}
+
+.images{
+    height:120px;
+}
 
 
 
--- src/routes/unified/+page.svelte
+++ src/routes/unified/+page.svelte
@@ -45,11 +45,110 @@
         <img class= "img1" src ="\images\Unified Shopping cart banner illustration.png" alt="banner">
         </div>
 
+        <div class = "exp">
+            <img class ="expimage" src = "\images\Unified Shopping Cart Image.jpg" alt = "experience">
+            <div class = "exppara">
+                <h2>UNIFIED <br/>SHOPPING<br/>EXPERIENCE</h2>
+                <p>Unified cart lets your users easily buy anything —<br/>
+                    from booking lodging or experiences to buying<br/>
+                    from your store — from one cart! This way, you<br/>
+                    can set up multiple integrations, with consistent<br/>
+                    cart and order behaviour across the board.</p>
+            </div>
+        </div>
 
+        <div class="how">
+            <div class="howpara">
+                <h2>HOW IT WORKS</h2>
+                 <ul>
+                    <li><p>The user adds products to the cart.</p></li>
+                    <li><p>All products will be placed in one<br/>consolidated cart</p></li>
+                    <li><p>The user completes the checkout process</p></li>
+                    <li><p>You and the user will each recieve a single<br/>
+                        order confirmation. All integrated systems<br/>
+                        will be updated simultaneously.</p></li>
+                 </ul>
+            </div>
+            <img class="howpic" src="\images\How it works illustration.png" alt="howitworks">
+        </div>
+
+    <div class="features">
+        <h2>FEATURES</h2>
+    </div>
+
+        <div class="bottom1">
+            <div class="bot">
+                <img class="images" src = "\images\No Transaction Fees.jpg" alt = "NTF">
+                <h3>No Transaction Fees</h3>
+                <p>Where other carts skim off the top of your<br/>
+                    profits by charging a percentage fee for<br/>
+                    every transaction, our platform does not<br/>
+                    leverage fees on top of those normally<br/>
+                    charged by your payment processor.</p>
+            </div>
+            <div class="bot">
+                <img class="images" src = "\images\No Product Limits.jpg" alt = "NPL">
+                <h3>No Product Limits</h3>
+                <p>Customers are able to purchase as many<br/>
+                    products to the unified shopping cart as they<br/>
+                    desire. </p>
+            </div>
+        </div>
+
+        <div class="bottom2">
+            <div class="bot">
+                <img class="images" src = "\images\Present Customers With a Seamless, Bug-free Cart.jpg" alt = "bugfree">
+                <h3>Present Customers With <br/>Seamless, Bug-free Cart</h3>
+                <p>With Go4Commerce, you get the best<br/>
+                    shopping cart features in the industry to<br/>
+                    ensure your customers have the smoothest<br/>
+                    experience possible - and it's completely<br/>
+                    bug-free, as well as designed to make<br/>
+                    shopping easy for them along the way.</p>
+            </div>
+            <div class="bot">
+                <img class="images" src = "\images\Unified Invoicing.jpg" alt = "NPL">
+                <h3>No Product Limits</h3>
+                <p>All orders made will appear on one single<br/>
+                    unified invoice, making it easier for you and<br/>
+                    your customer to manage orders. </p>
+            </div>
+        </div>
+        
+        <div class="bottom3">
+            <div class="bot">
+                <img class="images" src = "\images\Unified Payment.jpg" alt = "UP">
+                <h3>Unified Payment</h3>
+                <p>With the Unified Shopping Cart, customers<br/>
+                    can purchase products using one payment<br/>
+                    gateway and one payment process. </p>
+            </div>
+            <div class="bot">
+                <img class="images" src = "\images\Customizable Invoice.jpg" alt = "CI">
+                <h3>Customizable Invoice</h3>
+                <p>Have the freedrom to customize your own<br/>
+                    invoice, giving your customers a<br/>
+                    omnichannel experience.</p>
+            </div>
+        </div>
+
+        <div class = "footer">
+            <logo2><a href = "/"><img src="\images\go4logo.png" alt="logo" style=height:40px></a></logo2>
+        
+                        
+                        
+        </div>
 </body>
 
 
 <style>
+
+p{
+    padding-bottom: 20px;
+}
+    h2{
+        padding-bottom: 20px;
+    }
 .unified{
     display: flex;
     justify-content: space-around;
@@ -60,12 +159,60 @@
     padding-top: 80px;
 }
 
+.exp{
+    display: flex;
+    justify-content: center;
+    padding-top: 100px;
+}
+
+.expimage{
+    padding-right:30px;
+}
+.exppara{
+    padding-top: 50px;
+}
+
+.how{
+    display:flex;
+    justify-content:space-evenly;
+    padding-top: 100px;
+}
+
+.howpara{
+    padding-top: 80px;
+    padding-left:50px;
+}
+
+.howpara li{
+    list-style-image: url('/images/Check mark.jpg');
+    
+}
+
+.features{
+    text-align: center;
+    padding-top: 100px;
+}
 
 
+.bot{
+    text-align: center;
+}
+.bottom1{
+    display: flex;
+    justify-content: space-evenly;
+    padding-top: 50px;
+}
 
+.bottom2{
+    display: flex;
+    justify-content: space-evenly;
+    padding-top: 50px;
+}
 
-
-
-
-
+.bottom3{
+    display: flex;
+    justify-content: space-evenly;
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
 </style>
